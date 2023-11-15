e66e6bce4dfc66db7aaac100b5f151a2a3cc96d8
Billy He <billy@metalabdesign.com>
2023-11-15 10:50:59 -0800

    Fix a 24-to-12 hours value conversion error

--- src/functions/slash-command/default.ts
+++ src/functions/slash-command/default.ts
@@ -43,7 +43,7 @@ const SCHEDULE_STRING_REGEXP_24H = new RegExp(
 
 const stringifyNormalizedTime = (hour24: number): [string, string] => {
   const hour12 = hour24 > 12 ? hour24 - 12 : hour24;
-  const suffix12 = hour24 > 12 ? "pm" : "am";
+  const suffix12 = hour24 >= 12 ? "pm" : "am";
   return [
     `${hour24.toString().padStart(2, "0")}:00`,
     `${hour12.toString().padStart(2, "0")}:00${suffix12}`
