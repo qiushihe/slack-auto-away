0288a55c48d8ec1c410ac50bf5e22e3cfc143c8a
Billy He <billy@metalabdesign.com>
2024-02-09 12:12:14 -0800

    Add TODO about implementing scheduled updates

--- src/functions/schedule/apply.ts
+++ src/functions/schedule/apply.ts
@@ -3,9 +3,13 @@ import { Handler } from "aws-lambda";
 export const handler: Handler<never> = async (evt) => {
   console.log("[schedule/apply] Event: ", evt);
 
+  // TODO: Implement logic to find all users who are due to either turn on or off their `away`
+  //       status, and make that happen via Slack's user presence API.
+  //       See: https://api.slack.com/methods/users.setPresence
+
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: "It Scheduled!" })
+    body: JSON.stringify({ message: "It Worked!" })
   };
 };
